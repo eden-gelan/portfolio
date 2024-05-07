5a1a6cd5dbb21dc98d100ad2eb3e48c9e4790a74
Eden Gelan <edigelan123@gmail.com>
2024-05-06 23:45:52 -0800

    new3 update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,17 +5,14 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="min-h-[500px] ">
-      <div className=" max-w-[900px] mx-auto">
+      <div className=" max-w-[1000px] mx-auto">
         <div className="flex  justify-between items-center  pt-24 px-4 gap-8 flex-col md:flex-row">
           <div className="flex flex-col gap-4 flex-1 upAnimate">
             <h3 className="t text-4xl font-extrabold tracking-normal">
               Hello, I'm Eden ッ
             </h3>
             <h4>
-              I am an intermediate application designer and mobile app
-              developer, specializing in interaction design and design systems.
-              With a comprehensive grasp of the end-to-end product design
-              process.
+            I'm a designer who makes websites and apps look great and easy to use. I love creating visuals that catch the eye and designing layouts that make sense for users. Whether it's crafting logos, designing interfaces, or ensuring smooth navigation, I enjoy making digital experiences both beautiful and functional.
             </h4>
           </div>
           <div className="flex-1">
